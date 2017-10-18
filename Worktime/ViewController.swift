9fd51e0de4c8ef72fbcf5f7a36da6587fb0c28c9
Lucas Nascimento <lucas.nascimento@pearson.com>
2017-10-17 22:14:23 -0200

    Initial Commit

---
+++ Worktime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Worktime
+//
+//  Created by Lucas Nascimento on 17/10/17.
+//  Copyright © 2017 Lucas Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
